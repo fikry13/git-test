ca8a7e9faca8262dbc9f1afcee338ee1a942d662
Fikry Abdullah Aziz <fikry13@gmail.com>
2022-09-05 23:44:17 +0700

    Test commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public Text testText;
+
+    private int num;
+
+    private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer >= 2)
+        {
+            num+=2;
+            timer = 0;
+            testText.text = num.ToString();
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+    }
+}
